2ca4e3c6b34ea36111cbdeafb6a4595c1089466c
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-12 21:11:00 -0400

    created module for connect function

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541',
+  });
+
+  conn.on("data", (data) => {
+    console.log("Note from Server: ", data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,21 +1,7 @@
-const net = require("net");
+// const net = require("net");
+const connect = require("./client");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
-  });
-
-  conn.on("data", (data) => {
-    console.log("Note from Server: ", data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
